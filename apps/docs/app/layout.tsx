e88aa9a3cf6bee931f3ea55b278b0730e08ea45b
Mohamad Alkhatib <mohammad.alkhatib@web.de>
2026-01-01 15:19:48 +0100

    Fixed docs repository base url

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -104,7 +104,7 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/nulldoubt/Pepperminto"
+          docsRepositoryBase="https://github.com/nulldoubt/pepperminto/tree/main/apps/docs"
           sidebar={sidebar}
         >
           {children}
